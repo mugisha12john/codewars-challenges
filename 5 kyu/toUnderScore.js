cff630760a41e135462c8c1d761ffab5ce513353
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-07 10:00:39 +0200

    feat: add regex and also if input is number return as string

--- 5 kyu/toUnderScore.js
+++ 5 kyu/toUnderScore.js
@@ -1,28 +1,26 @@
+// Complete the function/method so that it takes a PascalCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If the method gets a number as input, it should return a string.
+
+// Examples
+// "TestController"  -->  "test_controller"
+// "MoviesAndBooks"  -->  "movies_and_books"
+// "App7Test"        -->  "app7_test"
+// 1                 -->  "1"
 function toUnderscore(string) {
-  // const upperLetter = string.match(/[A-Z]/g)
-  // const searchposition = [string.indexOf(upperLetter[0]),string.indexOf(upperLetter[1])]
- 
-//   for(let i=0;i<=string.length;i++){
-//     let a = string[i].toString().toUpperCase()
-//     if(string[i] === a){
-//         console.log("Underscore at index" + i)
-//     }
-//   }
+    if(typeof(string) === 'number') return String(string)
  let arr = string.split('')
  let result =''
  for(letter of arr){
-     
-    if(letter === letter.toUpperCase()){
+    if(/\d/.test(letter)){
+        result+=letter
+    }
+    else if(letter === letter.toUpperCase()){
         result+=`_${letter.toLowerCase()}`
     }else{
         result+=letter
     }
  
  }
- return result
+ return (/^_/.test(result) ? result.slice(1) : result)
 }
-// "TestController"  -->  "test_controller"
-// "MoviesAndBooks"  -->  "movies_and_books"
-// "App7Test"        -->  "app7_test"
-// 1                 -->  "1"
-console.log(toUnderscore("TestController"))
+console.log(toUnderscore("Test4C432ontroller"))
+console.log(toUnderscore("MoviesAndBooks"))
