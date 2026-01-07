eeb5608017eff1521828a020fbc987cd66d91b54
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-07 16:34:54 +0200

    feat: luck check

---
+++ 5 kyu/luckCheck.js
@@ -0,0 +1,41 @@
+function luckCheck(ticket) {
+  if (/[A-Za-g]/gi.test(ticket)) {
+    throw Error("expected [Function] to throw an error");
+  }
+  if (ticket.length % 2 === 0) {
+    let a = ticket.length / 2;
+    let leftSide = ticket
+      .slice(0, a)
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a + b, 0);
+    let rightSide = ticket
+      .slice(a)
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a + b, 0);
+    return rightSide === leftSide ? true : false;
+  } else {
+    let middle = Math.round(ticket.length / 2);
+    let leftSide = ticket
+      .slice(0, middle - 1)
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a + b, 0);
+    let rightSide = ticket
+      .slice(middle)
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a + b, 0);
+    return rightSide === leftSide ? true : false;
+  }
+}
+// 17935     #         1 + 7 = 3 + 5  // if the length is odd, you should ignore the middle number when adding the halves.
+console.log(luckCheck("683179"));
+console.log(luckCheck("17935"));
+console.log(luckCheck("56328116"));
+console.log(luckCheck("003111"));
