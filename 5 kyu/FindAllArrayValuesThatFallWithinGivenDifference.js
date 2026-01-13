859ae64518de6b407143e756c3717a0e99c05be4
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-13 18:27:45 +0200

    feat: using way of create the variable to hold next and prev and check if difference is in given range and if yes keep current iteration

--- 5 kyu/FindAllArrayValuesThatFallWithinGivenDifference.js
+++ 5 kyu/FindAllArrayValuesThatFallWithinGivenDifference.js
@@ -4,14 +4,25 @@ class GroupByDifference {
   }
   find(range){
     let result = []
-    this.numbers.forEach((element,index) => {
-        let sum = element - (this.numbers[index+1] || 0)
-        if(Math.abs(sum) <= range){
-            result.push([element,this.numbers[index+1]])
-        }
+    // this.numbers.forEach((element,index) => {
+    //     let sum = element - (this.numbers[index+1] || 0)
+    //     if(Math.abs(sum) <= range){
+    //        result.push([element,this.numbers[index+1]])
+    //     } 
+       
+    // });
+        this.numbers.forEach((element,index) => {
+            let lastNumber = this.numbers[index-1]
+            let nextNumber = this.numbers[index+1]
+            let matchLastNumber = lastNumber !== undefined && Math.abs(element - lastNumber)<= range
+            let nextMatchNumber = nextNumber !== undefined && Math.abs(element - nextNumber) <= range
+            if(matchLastNumber || nextMatchNumber){
+                result.push(element)
+            }
        
     });
-    return result.flat(Infinity)
+    
+    return result
   }
 }
 
@@ -22,3 +33,5 @@ class GroupByDifference {
 // [5, 5, 30, 31, 32]
 const  numbers = new GroupByDifference([5, 32, 5, 1, 31, 70, 30, 8])
 console.log(numbers.find(2))
+// .find(3), [5,5,8,30,31,32]
+// console.log(numbers.find(3))
