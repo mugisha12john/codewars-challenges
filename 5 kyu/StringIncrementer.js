c1b375f91141301afda398a9144250f5c5953d42
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-16 08:58:14 +0200

    faet: string incrementer

---
+++ 5 kyu/StringIncrementer.js
@@ -0,0 +1,45 @@
+// Your job is to write a function which increments a string, to create a new string.
+
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+function incrementString(strng) {
+  let regex = /[0-9]/gi;
+  let num = strng.match(regex);
+  let stringOnly = strng.match(/[A-Za-z]/gi).join("");
+  if (num === null) {
+    num = 1;
+    return `${stringOnly}${num}`;
+  } else {
+    let iteration = Number(num.join("")) + 1;
+    let currentLen = String(iteration).length;
+    if (num.length === 1) {
+      return `${stringOnly}${iteration}`;
+    } else if (currentLen !== num.length) {
+      let missing = num.length - currentLen;
+       missing = Math.max(0, missing);
+      let zeros = "0".repeat(missing);
+      return `${stringOnly}${zeros}${iteration}`;
+    } else {
+      return `${stringOnly}${iteration}`;
+    }
+  }
+}
+console.log(incrementString('foo'))
+console.log(incrementString('foobar23'))
+console.log(incrementString('foo0043'))
+console.log(incrementString('foo099'))
+console.log(incrementString("foo9"));
+console.log(incrementString('foobar000'))
--- promise.js
+++ promise.js
@@ -2,18 +2,18 @@
 // the request a specified number of times if it fails.
 //  ( Bonus question for preparing the practical mental model).
 
-// async function getData() {
-//     let num = 0;
-//   let api = await fetch("https://api.github.com/users/mugisha12ujohn");
-//   while (!api.ok && num<=3) {
-//     // let retry = await fetch("https://api.github.com/users/muggisha12ohn");
-//     // api = retry;
-//     console.log("am failed")
-//     num++
-//   }
-//   console.log("good job");
-// }
-// getData();
+async function getData() {
+    let num = 0;
+  let api = await fetch("https://api.github.com/users/mugisha12ujohn");
+  while (!api.ok && num<=3) {
+    // let retry = await fetch("https://api.github.com/users/muggisha12ohn");
+    // api = retry;
+    console.log("am failed")
+    num++
+  }
+  console.log("good job");
+}
+getData();
 
 async function getData() {
   let num = 0;
