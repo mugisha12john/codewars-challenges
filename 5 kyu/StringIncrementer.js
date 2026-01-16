57d5950059690114a3b0d32aa4e003c15ae96d81
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-01-16 15:43:55 +0200

    faet: string incrementer modify regex

--- 5 kyu/StringIncrementer.js
+++ 5 kyu/StringIncrementer.js
@@ -16,27 +16,34 @@
 
 // Attention: If the number has leading zeros the amount of digits should be considered.
 function incrementString(strng) {
-  let regex = /[0-9]/gi;
-  let num = strng.match(regex);
-  let stringOnly = strng.match(/[A-Za-z]/gi).join("");
+  if (strng === null) return;
+
+  let num = strng.match(/\d+$/);
+  
+  
+  let stringOnly = num ? strng.slice(0, -num[0].length) : strng;
+
   if (num === null) {
-    num = 1;
-    return `${stringOnly}${num}`;
+    return `${stringOnly}1`;
   } else {
-    let iteration = Number(num.join("")) + 1;
+    let iteration = Number(num[0]) + 1;
     let currentLen = String(iteration).length;
-    if (num.length === 1) {
+
+    if (num[0].length === 1) {
       return `${stringOnly}${iteration}`;
-    } else if (currentLen !== num.length) {
-      let missing = num.length - currentLen;
-       missing = Math.max(0, missing);
+    } 
+    else if (currentLen !== num[0].length) {
+      let missing = num[0].length - currentLen;
+      missing = Math.max(0, missing);
       let zeros = "0".repeat(missing);
       return `${stringOnly}${zeros}${iteration}`;
-    } else {
+    } 
+    else {
       return `${stringOnly}${iteration}`;
     }
   }
 }
+
 console.log(incrementString('foo'))
 console.log(incrementString('foobar23'))
 console.log(incrementString('foo0043'))
