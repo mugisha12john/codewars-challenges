166f6230d0124689a9fc203d4a49aa3a74577031
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-11 13:49:02 +0200

    feat: nozero for here

---
+++ typescript/8 kyu/NozeroForHero.ts
@@ -0,0 +1,17 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450   -> 145
+// 960000 -> 96
+// 1050   -> 105
+// -1050  -> -105
+// 0      -> 0
+// Note: Zero should be left as it is.
+export function noBoringZeros(n: number):number {
+  return Number(n.toString().replace(/0+$/,""))
+}
+
+console.log(noBoringZeros(1050000));
