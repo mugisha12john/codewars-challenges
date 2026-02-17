8dc614ad1dc13a50d4cd02e2d14642b21d163ac6
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-17 16:54:46 +0200

    feat: basic mathematics operation

---
+++ typescript/8 kyu/BasicMathematicalOperations.ts
@@ -0,0 +1,18 @@
+export function basicOp(operation: string, value1: number, value2: number):number {
+  if(operation === '+'){
+    return value1 + value2
+  }
+  else if(operation === '*'){
+    return value1 * value2
+  }
+  else if(operation === '-'){
+    return value1 - value2
+  }
+  else if(operation === '/'){
+    return value1 / value2
+  }
+  else{
+    return 0
+  }
+}
+console.log(basicOp('+', 4, 7))
