f76c60e5d6d25c9d49df4bfff1ffb032067e33ad
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-23 14:13:41 +0200

    feat: bumps in the road

---
+++ typescript/7 kyu/Bumps in the Road.ts
@@ -0,0 +1,12 @@
+// Your car is old, it breaks easily. The shock absorbers are gone and you think it 
+// can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either 
+// flat road (_) or bumps (n). If you are able to reach home safely by 
+// encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+export function bump(x: string){
+  return x.split('')
+}
+console.log(bump("__nn_nnnn__n_n___n____nn__nnn"))
+// bump("__nn_nnnn__n_n___n____nn__nnn") =>  "Woohoo!"
+// bump("nnnnnnnnnnnnnnnnnnnnn"), "Car Dead")
