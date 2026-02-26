c61a14c00ec6ebe8415b123f68e3f597c4397c88
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-26 11:02:25 +0200

    feat: build a square adding codewars question describition

--- typescript/7 kyu/Build a square.ts
+++ typescript/7 kyu/Build a square.ts
@@ -1,3 +1,11 @@
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
 export function generateShape(int: number): string {
   let result: string = '';
   for (let i = 1; i <= int; i++) {
