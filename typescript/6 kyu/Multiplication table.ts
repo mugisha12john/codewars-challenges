8a9be22e4b1958406d89a54a9ecc5c26d2926de3
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-02-26 10:42:36 +0200

    feat: using square pattern to perfom the multiplication table

--- typescript/6 kyu/Multiplication table.ts
+++ typescript/6 kyu/Multiplication table.ts
@@ -8,6 +8,19 @@
 // For the given example, the return value should be:
 
 // [[1,2,3],[2,4,6],[3,6,9]]
-export function multiplicationTable (size: number): number[][] {
-  // Implement me! :)
+export function multiplicationTable(size: number) {
+    let result:number[][] = []
+  let start =1;
+  for (let i = 1; i <= size; i++) {
+    let row:number[] = [];
+    for (let j = 1; j <= size; j++) { 
+    
+      row.push(start*j)
+    }
+    start++
+     result.push(row)
+  }
+  return result
 }
+console.log(multiplicationTable(3));
+console.log(multiplicationTable(2))
