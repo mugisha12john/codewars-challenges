28da94ea6829b56e7806131c6602eba68eff416b
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-03 12:44:13 +0200

    feat: check if score is less than zero then return 0

--- typescript/7 kyu/Check the exam.ts
+++ typescript/7 kyu/Check the exam.ts
@@ -1,16 +1,16 @@
 // The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"].
 //  The second one contains a student's submitted answers.
 
-// The two arrays are not empty and are the same length. 
+// The two arrays are not empty and are the same length.
 // Return the score for this array of answers, giving +4 for each correct answer,
-//  -1 for each incorrect answer, and +0 for each blank answer, represented 
+//  -1 for each incorrect answer, and +0 for each blank answer, represented
 // as an empty string (in C the space character is used).
 
 // If the score < 0, return 0.
 
 // For example:
 
-//     Correct answer    |    Student's answer   |   Result         
+//     Correct answer    |    Student's answer   |   Result
 //  ---------------------|-----------------------|-----------
 //  ["a", "a", "b", "b"]   ["a", "c", "b", "d"]  →     6
 //  ["a", "a", "c", "b"]   ["a", "a", "b", "" ]  →     7
@@ -18,15 +18,21 @@
 //  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
 
 export function checkExam(array1: string[], array2: string[]): number {
-    let score:number = 0
-    array1.forEach((v,i)=>{
-        if(v === array2[i]){
-            score+=4
-        }
-        else if(v !== array2[i]){
-            score-=1
-        }
-    })
- return score
+  let score: number = 0;
+  array1.forEach((v, i) => {
+    if (v === array2[i]) {
+      score += 4;
+    } else if (v !== array2[i] && array2[i] === "") {
+      score += 0;
+    } else if (v !== array2[i]) {
+      score -= 1;
+    } 
+    else {
+      return 0;
+    }
+  });
+  return score <0? 0:score;
 }
-console.log(checkExam(["a", "a", "b", "b"] ,["a", "c", "b", "d"] ))
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]));
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]));
+console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]))
