7e30b86de0c87407e750272a5309440714acec60
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-11 21:32:09 +0200

    feat: comment

--- typescript/5 kyu/Valid Braces.ts
+++ typescript/5 kyu/Valid Braces.ts
@@ -1,6 +1,24 @@
 export function validBraces(braces: string) {
   if (!/[(){}\[\]]/gim.test(braces)) return false;
-  
+  let match = []
+  let pair = {
+    '[': ']',
+    '(':')',
+    '{':'}'
+  }
+
+  // for(let str in braces){
+  //   if('({['.includes(str)){
+  //       match.push(str)
+  //   }
+  //   else{
+  //       if(!match || match[-1] !== pair[str])
+  //   }
+  // }
+//   return braces.split('').reduce<Record<string,number>>((a,b)=> {
+//     a[b] = (a[b] || 0) +1
+//     return a
+//   },{})
 }
 // "(){}[]"   =>  True
 // "([{}])"   =>  True
@@ -8,3 +26,4 @@ export function validBraces(braces: string) {
 // "[(])"     =>  False
 // "[({})](]" =>  False
 console.log(validBraces("adf"));
+console.log(validBraces("(){}[]"))
