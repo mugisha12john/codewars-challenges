be4dc5e3d7c191748d99cc1f74ccf451593128c5
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-12 09:56:51 +0200

    feat: Base64Encoding

---
+++ typescript/5 kyu/Base64 Encoding.ts
@@ -0,0 +1,7 @@
+export function toBase64(data: Uint8Array): string {
+  // Encode the array to Base64
+}
+
+export function fromBase64(encoded: string): Uint8Array {
+  // Decode to an array from Base64
+}
