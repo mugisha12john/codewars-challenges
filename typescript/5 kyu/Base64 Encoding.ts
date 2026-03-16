4550de3ead913d945e5e203e5e3ee3d917205e4d
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-16 17:00:58 +0200

    feat: base64 have object of all required letters and vowels

--- typescript/5 kyu/Base64 Encoding.ts
+++ typescript/5 kyu/Base64 Encoding.ts
@@ -1,7 +1,27 @@
-export function toBase64(data: Uint8Array): string {
-  // Encode the array to Base64
+export function toBase64(data: Uint8Array) {
+  let obj: { [key: number]: string }[] = [];
+  let obj = [];
+  let counter = 0;
+
+  // Uppercase A-Z (26 chars)
+  for (let i = 65; i <= 90; i++) {
+    obj.push({ [counter++]: String.fromCharCode(i) });
+  }
+
+  // Lowercase a-z (26 chars)
+  for (let i = 97; i <= 122; i++) {
+    obj.push({ [counter++]: String.fromCharCode(i) });
+  }
+
+  // Digits 0-9 (10 chars)
+  for (let i = 48; i <= 57; i++) {
+    obj.push({ [counter++]: String.fromCharCode(i) });
+  }
+
+  console.log(obj);
 }
 
-export function fromBase64(encoded: string): Uint8Array {
-  // Decode to an array from Base64
-}
+toBase64("this is a bytestring!this is a bytestring!");
+// export function fromBase64(encoded: string): Uint8Array {
+//   // Decode to an array from Base64
+// }
