dbd2a100d23ed184190becc2bfd18b0fa0be0847
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2026-03-17 17:27:26 +0200

    feat: create pairs

--- typescript/5 kyu/Base64 Encoding.ts
+++ typescript/5 kyu/Base64 Encoding.ts
@@ -1,27 +1,36 @@
 export function toBase64(data: Uint8Array) {
   let obj: { [key: number]: string }[] = [];
-  let obj = [];
   let counter = 0;
-
-  // Uppercase A-Z (26 chars)
+  let binary: object[] = [];
+  // Uppercase A-Z
   for (let i = 65; i <= 90; i++) {
     obj.push({ [counter++]: String.fromCharCode(i) });
   }
-
-  // Lowercase a-z (26 chars)
+  // Lowercase a-z
   for (let i = 97; i <= 122; i++) {
     obj.push({ [counter++]: String.fromCharCode(i) });
   }
 
-  // Digits 0-9 (10 chars)
+  // Digits 0-9
   for (let i = 48; i <= 57; i++) {
     obj.push({ [counter++]: String.fromCharCode(i) });
   }
 
-  console.log(obj);
+  obj.push({ [counter++]: "+" });
+  obj.push({ [counter++]: "/" });
+  for (let i = 0; i < data.length; i += 3) {
+    const chunk = data.slice(i, i + 3);
+    binary.push(chunk);
+  }
+//   const binaryArray = binary.map((group) =>
+//     group.map((num) => num.toString(2).padStart(8, "0")).join(""),
+//   );
+
+//   console.log(binaryArray);
 }
 
-toBase64("this is a bytestring!this is a bytestring!");
+//out put =   dGhpcyBpcyBhIHRlc3Qh
+console.log(toBase64([77, 97, 110, 32, 65]));
 // export function fromBase64(encoded: string): Uint8Array {
 //   // Decode to an array from Base64
 // }
