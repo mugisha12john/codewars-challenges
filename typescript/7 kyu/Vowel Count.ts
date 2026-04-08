bab59719d9ea41694a6873f52e8078b9084a75f5
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-08 18:33:34 +0200

    feat: vowel count final solution

--- typescript/7 kyu/Vowel Count.ts
+++ typescript/7 kyu/Vowel Count.ts
@@ -3,3 +3,18 @@
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 // The input string will only consist of lower case letters and/or spaces.
+export class Kata {
+   getCount(str: string):number {
+    let vowel = ['a','i','o','u','e']
+    let count =0
+    for(let i=0;i<str.length;i++){
+        if(vowel.includes(str[i])){
+            count+=1
+        }
+    }
+    return count
+}
+}
+// Kata.getCount("abracadabra"), 5)
+const k = new Kata()
+console.log(k.getCount('abracadabra'))
