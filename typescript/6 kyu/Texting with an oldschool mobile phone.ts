11761bbaff233c2f85b568776b0a88766aa7f6d2
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-13 16:40:11 +0200

    feat: up an down

--- typescript/6 kyu/Texting with an oldschool mobile phone.ts
+++ typescript/6 kyu/Texting with an oldschool mobile phone.ts
@@ -1,6 +1,10 @@
-// If you're old enough, you might remember buying your first mobile phone, one of the old ones with no touchscreen, and sending your first text message with excitement in your eyes. Maybe you still have one lying in a drawer somewhere.
+// If you're old enough, you might remember buying your first mobile phone, one of the old ones with no
+//  touchscreen, and sending your first text message with excitement in your eyes. Maybe you still have one
+// lying in a drawer somewhere.
 
-// Let's try to remember the good old days and what it was like to send text messages with such devices. A lot of them had different layouts, most notably for special characters and spaces, so for simplicity we'll be using a fictional model with 3x4 key layout shown below:
+// Let's try to remember the good old days and what it was like to send text messages with such devices.
+//  A lot of them had different layouts, most notably for special characters and spaces, so for simplicity
+// we'll be using a fictional model with 3x4 key layout shown below:
 
 // -------------------------
 // |   1   |   2   |   3   |  <-- hold a key to type a number
@@ -29,19 +33,24 @@
 
 // 2 => 'a', 9999 => 'z', 111 => '?', *** => '+'
 // Typing numbers
-// To type numbers 0-9 and special characters *# - hold that key. Holding is represented by a number, followed by a dash. Examples:
+// To type numbers 0-9 and special characters *# - hold that key. Holding is represented by a number,
+//  followed by a dash. Examples:
 
 // 3- => '3', 5-5-5- => '555'
 // Uppercase / Lowercase
-// Initially the case is lowercase. To toggle between lowercase and uppercase letters, use the # symbol. Case switching should only be considered when next character is alphabetic (a-z). Examples:
+// Initially the case is lowercase. To toggle between lowercase and uppercase letters,
+// use the # symbol. Case switching should only be considered when next character is alphabetic (a-z). Examples:
 
 // #2#9999 => 'Az' (remember, it's a toggle)
 // 27-#2255 => 'a7BK' (do not switch before '7')
 // Waiting for next character
-// If you have 2 or more characters in a sequence that reside on the same button (refer to layout, bottom row), you have to wait before pressing the same button again. Waiting is represented by adding a space between 2 (or more) such characters. Example:
+// If you have 2 or more characters in a sequence that reside on the same button (refer to layout, bottom row),
+//  you have to wait before pressing the same button again. Waiting is represented by adding a space
+// between 2 (or more) such characters. Example:
 
 // 44 444 44 444 => 'hihi'
-// Exceptions: No need to wait after holding any key, even if next character resides on same button (4-4 => '4g'), or if there's a case switch between 2 characters on same button (#5#55 => 'Jk').
+// Exceptions: No need to wait after holding any key, even if next character resides on same button (4-4 => '4g'),
+//  or if there's a case switch between 2 characters on same button (#5#55 => 'Jk').
 
 // Example
 // To put it all together, let's go over an example. Say you want to type this message - 'Def Con 1!':
@@ -52,12 +61,68 @@
 // In a similar manner type the second word (space is located on number 0). 0#222#666 660 => ' Con '
 // Finish off by holding 1 as 1- and typing ! as 1111 ('1-1111' = 1!). Note that after holding a key you don't need to wait to press another key.
 // Result:
-// sendMessage("Def Con 1!") => "#3#33 3330#222#666 6601-1111" 
+// sendMessage("Def Con 1!") => "#3#33 3330#222#666 6601-1111"
 
 // More examples are provided in sample test suite.
 
 // All inputs will be valid strings and only consist of characters from the key layout table.
 export function sendMessage(message: string): string {
-  // Click click click
+  const keyMap: Record<string, string> = {
+    a: "2",
+    b: "22",
+    c: "222",
+    d: "3",
+    e: "33",
+    f: "333",
+    g: "4",
+    h: "44",
+    i: "444",
+    j: "5",
+    k: "55",
+    l: "555",
+    m: "6",
+    n: "66",
+    o: "666",
+    p: "7",
+    q: "77",
+    r: "777",
+    s: "7777",
+    t: "8",
+    u: "88",
+    v: "888",
+    w: "9",
+    x: "99",
+    y: "999",
+    z: "9999",
+
+    " ": "0",
+
+    ".": "1",
+    ",": "11",
+    "?": "111",
+    "!": "1111",
+    "'": "*",
+    "-": "**",
+    "+": "***",
+    "=": "****",
+  };
+
+   let result = '';
+  let currentCase: 'lower' | 'upper' = 'lower';
+  let previousKey: string | null = null;
+
+  for (let char of message) {
+    let output = '';
+
+    //  Numbers
+    if (/[0-9]/.test(char)) {
+      output = `${char}-`;
+      previousKey = null; 
+      result += output;
+      continue;
+    }
+  }
   return message;
 }
+
+console.log(sendMessage('5-5-5-'))
