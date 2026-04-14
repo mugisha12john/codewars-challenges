43f7af59da17193ce1c1c668552ba4c07effc5bd
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-14 18:21:03 +0200

    feat: is Sator square, using two for loops

--- typescript/7 kyu/Is Sator Square.ts
+++ typescript/7 kyu/Is Sator Square.ts
@@ -1,6 +1,19 @@
-export function isSatorSquare(tablet: string[][]): boolean {
-
-  //  <----  hajime!
-
-  return true || false;
+export function isSatorSquare(tablet: string[][]) {
+  const totalSize = tablet.length;
+  for (let i = 0; i < totalSize; i++) {
+    for (let j = 0; j < totalSize; j++) {
+        if(tablet[i][j] !== tablet[j][i] || tablet[i][j] !== tablet[totalSize-i-1][totalSize-j-1]){
+            return false
+        }
+    }
+  }
+  return true
 }
+console.log(
+  isSatorSquare([
+    ["B", "A", "T", "S"],
+    ["A", "B", "U", "T"],
+    ["T", "U", "B", "A"],
+    ["S", "T", "A", "B"],
+  ]),
+);
