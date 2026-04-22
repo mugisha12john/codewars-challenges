250832870a0c8c666439142c0ae8bbbf3d0f791d
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-04-22 13:26:37 +0200

    feat: all inclusive

---
+++ typescript/7 kyu/All Inclusive.ts
@@ -0,0 +1,8 @@
+export function containAllRots(str: string, arr: string[]): boolean {
+  return true
+}
+// contain_all_rots(
+//   "bsjq", ["bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs"]) -> true
+
+// contain_all_rots(
+//   "Ajylvpy", ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"]) -> false)
