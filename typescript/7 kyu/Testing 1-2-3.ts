105d7400fbdbcb46d1ee913cef6c7e5cbf6e755d
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-05-13 14:35:15 +0200

    refactor: simplify number function using map for better readability

--- typescript/7 kyu/Testing 1-2-3.ts
+++ typescript/7 kyu/Testing 1-2-3.ts
@@ -1,8 +1,11 @@
 export function number(array: string[]):string[]{
-    let res:string[] = []
-    for(let i=0;i<array.length;i++){
-        res.push(`${i+1}: ${array[i]}`)
-    }
-    return res
+    // let res:string[] = []
+    // for(let i=0;i<array.length;i++){
+    //     res.push(`${i+1}: ${array[i]}`)
+    // }
+    // return res
+      return array.map((letter,index)=>{
+    return `${index+1}: ${letter}`
+  })
 }
 console.log(number(["a", "b", "c"]))
