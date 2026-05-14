9fe2617a833bddfeb7ec5806e9f2395a3f8eacee
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-05-14 14:31:54 +0200

    refactor: streamline createPhoneNumber function by removing commented code and using regex for formatting

--- typescript/6 kyu/CreatePhoneNumber.ts
+++ typescript/6 kyu/CreatePhoneNumber.ts
@@ -6,11 +6,11 @@
 
 // Don't forget the space after the closing parentheses!
 export function createPhoneNumber(numbers: number[]) {
-  // const regex = /^[0-9]{3} [0-9]{3} [0-9]{4}$/gmi
-  // return '0784961277'.match(regex)
 
-  const num = numbers.join('')
-  return `(${num.slice(0,3)}) ${num.slice(3,6)}-${num.slice(6,10)}`
+  return numbers.join('').replace(/([0-9]{3})([0-9]{3})([0-9]{4})/,(_,f1,f2,f3) => `(${f1}) ${f2}-${f3}`)
+
+  // const num = numbers.join('')
+  // return `(${num.slice(0,3)}) ${num.slice(3,6)}-${num.slice(6,10)}`
 
 }
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
