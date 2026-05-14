dedab2d05b3e1fada00c0c0be6d533a74b67c48f
Jean Baptiste MUGISHA <johnbutista004@gmail.com>
2026-05-14 13:59:59 +0200

    fix: correct regex and return statement in createPhoneNumber function

--- typescript/6 kyu/CreatePhoneNumber.ts
+++ typescript/6 kyu/CreatePhoneNumber.ts
@@ -1,4 +1,13 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
 export function createPhoneNumber(numbers: number[]) {
-  return numbers.join('').toString().match(/^[0-9]{3}:[0-9]{3}:[0-9]{4}$/gmi)
+  const regex = /^[0-9]{3}:[0-9]{3}:[0-9]{4}$/gmi
+  return '0784961277'.match(/^[0-9]{3}[0-9]{3}/gm)
 }
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
